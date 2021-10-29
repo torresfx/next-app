21706f3cece4d3b7ab7d6499f01e803552ec8ea0
torresfx <torres.fxv@gmail.com>
2021-10-29 07:44:17 -0400

    add products page

---
+++ pages/products.js
@@ -0,0 +1,14 @@
+const Products = () => {
+  return (
+    <>
+      <h1>Cool Product 1</h1>
+      <p>This is a very cool product</p>
+      <p>$2.50</p>
+      <hr />
+      <h1>Cool Product 2</h1>
+      <p>This is an even cooler product</p>
+      <p>$250.50</p>
+    </>
+  );
+};
+export default Products;
